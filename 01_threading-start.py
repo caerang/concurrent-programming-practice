a52b1f9988326e0e96fee718c4270fefda0f48d7
Hyeongdo Lee <mylovercorea@gmail.com>
2018-09-12 13:47:21 +0900

    Add threading practice

---
+++ 01_threading-start.py
@@ -0,0 +1,10 @@
+import threading
+
+
+def print_something(something):
+    print(something)
+
+t = threading.Thread(target=print_something, args=('hell', ))
+t.start()
+print('thread started')
+t.join()
